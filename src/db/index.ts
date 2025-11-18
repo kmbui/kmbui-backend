79a5f7f6b2dd785f11a1b4de0dbe220de1b96fa2
thorbert-anson-shi <tobyas2005139@gmail.com>
2025-11-18 13:58:05 +0700

    [CHORE] Added environment variables documentation and S3 client setup

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,7 @@
 import { Config } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import * as dotenv from "dotenv";
+import { S3Client } from "bun";
 
 // INFO: Loads from .env by default. Usage of other .env.* files
 // should be configured here with the 'path' option
@@ -11,4 +12,11 @@ export const db = drizzle({
     url: process.env.DATABASE_URL,
     authToken: process.env.DATABASE_AUTH_TOKEN,
   } as Config,
+});
+
+export const s3Client = new S3Client({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  bucket: process.env.BUCKET_NAME,
+  endpoint: process.env.S3_ENDPOINT,
 });
